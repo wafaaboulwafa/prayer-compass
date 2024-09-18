6f61765f78c7a866433959c9a1e84737e5a04cd7
Wafa <wafaaboulwafa@hotmail.com>
2024-09-18 22:53:36 +0300

    Compass

--- components/compassView.js
+++ components/compassView.js
@@ -1,63 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet, Animated } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { Magnetometer } from "expo-sensors";
 import { calculateHeading } from "../utils/heading";
 import settings from "../constants/settings";
+import * as Location from "expo-location";
 
 const CompassView = () => {
   const subscription = useRef(null);
   const headingAdjustment = useRef(0);
-  const rotateValue = new Animated.Value(0);
+  const [heading, setHeading] = useState(0);
 
   const onCompass = (result) => {
-    setData(result);
-    let heading = Math.atan2(result.y, result.x) * (180 / Math.PI);
-    if (heading < 0) heading += 360;
-    heading += headingAdjustment.current;
-    Animated.timing(rotateValue, {
-      toValue: heading,
-      duration: 100,
-      useNativeDriver: false,
-    }).start();
+    try {
+      let newHeading = Math.atan2(result.y, result.x) * (180 / Math.PI);
+      if (newHeading < 0) newHeading += 360;
+      newHeading += headingAdjustment.current;
+      setHeading(newHeading);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted")
-        throw "Permission to access location was denied";
-      let loc = await Location.getCurrentPositionAsync({});
-      headingAdjustment.current = calculateHeading(
-        loc.lat,
-        loc.lng,
-        settings.meccaLocation.lat,
-        settings.meccaLocation.lng
-      );
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
 
-      subscription.current = Magnetometer.addListener(onCompass);
+        if (status !== "granted")
+          throw "Permission to access location was denied";
+
+        let { status: magnetoStatus } =
+          await Magnetometer.requestPermissionsAsync();
+
+        if (magnetoStatus !== "granted")
+          throw "Permission to access sensor was denied";
+
+        let loc = await Location.getCurrentPositionAsync({});
+        const headAdjustment = calculateHeading(
+          loc.coords.latitude,
+          loc.coords.longitude,
+          settings.meccaLocation.lat,
+          settings.meccaLocation.lng
+        );
+
+        headingAdjustment.current = headAdjustment;
+        subscription.current = Magnetometer.addListener(onCompass);
+      } catch (e) {
+        console.log(e);
+      }
     })();
     return () => {
       if (subscription.current !== null) subscription.current.remove();
     };
   }, []);
 
-  const rotateStyle = {
-    transform: [
-      {
-        rotate: rotateValue.interpolate({
-          inputRange: [0, 360],
-          outputRange: ["0deg", "360deg"],
-        }),
-      },
-    ],
-  };
-
   return (
     <View style={styles.container}>
       <View style={styles.compassContainer}>
-        <Animated.Image
+        <Image
           source={require("../assets/compass.png")}
-          style={[styles.compassImage, rotateStyle]}
+          style={[
+            styles.compassImage,
+            { transform: [{ rotate: `${heading}deg` }] },
+          ]}
         />
       </View>
     </View>
@@ -69,20 +74,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#000000",
   },
   compassContainer: {
     width: 250,
     height: 250,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
-    borderRadius: 125,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 3,
-    elevation: 5,
+    backgroundColor: "#040404",
   },
   compassImage: {
     width: 200,
