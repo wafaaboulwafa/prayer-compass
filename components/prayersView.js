98f126541e4d5ba81336d1ad7037f2ddfdd16de8
Wafa <wafaaboulwafa@hotmail.com>
2024-10-04 23:18:17 +0300

    Prayer times

--- components/prayersView.js
+++ components/prayersView.js
@@ -1,16 +1,55 @@
 import React from "react";
 import { Coordinates, CalculationMethod, PrayerTimes } from "adhan";
 import { View, StyleSheet, Text } from "react-native";
+import moment from "moment-hijri";
 
+const PryaerTime = ({ titleEn, titleAr, value }) => {
+  const options = { hour: "2-digit", minute: "2-digit", second: "2-digit" };
+  const time = new Intl.DateTimeFormat("en-US", options).format(value);
+  return (
+    <View style={styles.line}>
+      <Text style={styles.titleEn}>{titleEn}</Text>
+      <Text style={styles.timeValue}>{time}</Text>
+      <Text style={styles.titleAr}>{titleAr}</Text>
+    </View>
+  );
+};
 const PrayersView = ({ location }) => {
   if (!location) return null;
   const params = CalculationMethod.MoonsightingCommittee();
   const coordinates = new Coordinates(location.latitude, location.longitude);
   const prayerTimes = new PrayerTimes(coordinates, new Date(), params);
-  console.log(prayerTimes);
+  const hijriDate = moment().format("iYYYY/iM/iD");
   return (
     <View style={styles.container}>
-      <Text>{JSON.stringify(prayerTimes, null, 4)}</Text>
+      <Text style={styles.date}>{hijriDate}</Text>
+      <PryaerTime titleEn={"Fajr"} titleAr={"الفجر"} value={prayerTimes.fajr} />
+      <PryaerTime
+        titleEn={"Sunrise"}
+        titleAr={"الشروق"}
+        value={prayerTimes.sunrise}
+      />
+      <PryaerTime
+        titleEn={"Dhuhr"}
+        titleAr={"الظهر"}
+        value={prayerTimes.dhuhr}
+      />
+      <PryaerTime titleEn={"Asr"} titleAr={"العسر"} value={prayerTimes.asr} />
+      <PryaerTime
+        titleEn={"Sunset"}
+        titleAr={"الغروب"}
+        value={prayerTimes.sunset}
+      />
+      <PryaerTime
+        titleEn={"Maghrib"}
+        titleAr={"المفرب"}
+        value={prayerTimes.maghrib}
+      />
+      <PryaerTime
+        titleEn={"Isha"}
+        titleAr={"العشاء"}
+        value={prayerTimes.isha}
+      />
     </View>
   );
 };
@@ -21,6 +60,21 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  date: {},
+  line: {
+    width: "100%",
+    flexDirection: "row",
+    marginHorizontal: 20,
+  },
+  titleEn: {
+    flex: 1,
+  },
+  titleAr: {
+    flex: 1,
+  },
+  timeValue: {
+    flex: 2,
   },
 });
 
