97f7640251e4ac5eafcc352d04cef51cf9e97442
Wafa <wafaaboulwafa@hotmail.com>
2024-10-05 00:48:17 +0300

    times

--- components/prayersView.js
+++ components/prayersView.js
@@ -5,55 +5,74 @@ import moment from "moment-hijri";
 import prayerTitles from "../constants/prayerTitles";
 
 const PrayerTime = ({ titleEn, titleAr, value }) => {
-  const options = { hour: "2-digit", minute: "2-digit", second: "2-digit" };
+  const options = { hour: "2-digit", minute: "2-digit" };
   const time = new Intl.DateTimeFormat("en-US", options).format(value);
   return (
-    <View style={styles.line}>
-      <Text style={styles.titleEn}>{titleEn}</Text>
-      <Text style={styles.timeValue}>{time}</Text>
-      <Text style={styles.titleAr}>{titleAr}</Text>
+    <View style={styles.timeInstance}>
+      <Text style={styles.timeInstanceTitle}>
+        {titleEn} - {titleAr}
+      </Text>
+      <Text style={styles.timeInstanceValue}>{time}</Text>
     </View>
   );
 };
 const PrayersView = ({ location }) => {
   if (!location) return null;
+  const today = new Date();
   const params = CalculationMethod.UmmAlQura();
   const coordinates = new Coordinates(location.latitude, location.longitude);
-  const prayerTimes = new PrayerTimes(coordinates, new Date(), params);
-  const hijriDate = moment().format("iYYYY/iM/iD");
+  const prayerTimes = new PrayerTimes(coordinates, today, params);
+  const hijriDateEn = moment()
+    .locale("en-US")
+    .format("iYYYY / iMM - iMMMM / iDD");
+  const hijriDateAr = moment()
+    .locale("ar-SA")
+    .format("iYYYY / iMM - iMMMM / iDD");
+
+  const dayNameEn = today.toLocaleDateString("en-US", { weekday: "long" });
+  const dayNameAr = today.toLocaleDateString("ar-SA", { weekday: "long" });
+
   return (
     <View style={styles.container}>
-      <Text style={styles.date}>{hijriDate}</Text>
-      <PrayerTime
-        titleEn={prayerTitles.fajr.en}
-        titleAr={prayerTitles.fajr.ar}
-        value={prayerTimes.fajr}
-      />
-      <PrayerTime
-        titleEn={prayerTitles.sunrise.en}
-        titleAr={prayerTitles.sunrise.ar}
-        value={prayerTimes.sunrise}
-      />
-      <PrayerTime
-        titleEn={prayerTitles.dhuhr.en}
-        titleAr={prayerTitles.dhuhr.ar}
-        value={prayerTimes.dhuhr}
-      />
-      <PrayerTime
-        titleEn={prayerTitles.asr.en}
-        titleAr={prayerTitles.asr.ar}
-        value={prayerTimes.asr}
-      />
-      <PrayerTime
-        titleEn={prayerTitles.maghrib.en}
-        titleAr={prayerTitles.maghrib.ar}
-        value={prayerTimes.maghrib}
-      />
-      <PrayerTime
-        titleEn={prayerTitles.isha.en}
-        titleAr={prayerTitles.isha.ar}
-        value={prayerTimes.isha}
-      />
+      <Text style={styles.date}>{dayNameEn + " - " + dayNameAr}</Text>
+      <Text style={styles.date}>{hijriDateEn}</Text>
+      <Text style={styles.date}>{hijriDateAr}</Text>
+      <View style={styles.timesContainer}>
+        <View style={styles.timesContainerLine}>
+          <PrayerTime
+            titleEn={prayerTitles.fajr.en}
+            titleAr={prayerTitles.fajr.ar}
+            value={prayerTimes.fajr}
+          />
+          <PrayerTime
+            titleEn={prayerTitles.sunrise.en}
+            titleAr={prayerTitles.sunrise.ar}
+            value={prayerTimes.sunrise}
+          />
+          <PrayerTime
+            titleEn={prayerTitles.dhuhr.en}
+            titleAr={prayerTitles.dhuhr.ar}
+            value={prayerTimes.dhuhr}
+          />
+        </View>
+        <View style={styles.timesContainerLine}>
+          <PrayerTime
+            titleEn={prayerTitles.asr.en}
+            titleAr={prayerTitles.asr.ar}
+            value={prayerTimes.asr}
+          />
+          <PrayerTime
+            titleEn={prayerTitles.maghrib.en}
+            titleAr={prayerTitles.maghrib.ar}
+            value={prayerTimes.maghrib}
+          />
+          <PrayerTime
+            titleEn={prayerTitles.isha.en}
+            titleAr={prayerTitles.isha.ar}
+            value={prayerTimes.isha}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -65,20 +84,27 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  timesContainer: {
+    flex: 1,
+  },
   date: {},
-  line: {
-    width: "100%",
-    flexDirection: "row",
+  timeContainer: {
+    flex: 1,
+    flexDirection: "column",
     marginHorizontal: 20,
   },
-  titleEn: {
+  timesContainerLine: {
+    flexDirection: "row",
+  },
+  timeInstance: {
+    height: 100,
+    width: 100,
+  },
+  timeInstanceTitle: {
     flex: 1,
   },
-  titleAr: {
+  timeInstanceValue: {
     flex: 1,
-  },
-  timeValue: {
-    flex: 2,
   },
 });
 
